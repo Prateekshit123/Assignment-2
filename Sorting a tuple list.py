9cbba8332d4c4932e230a0fa65ae8447cec56a82
Prateekshit Kulkarni <100770488+Prateekshit123@users.noreply.github.com>
2022-03-25 22:57:34 +0530

    Delete Sorting a tuple list.py

--- Sorting a tuple list.py
+++
@@ -1,7 +0,0 @@
-tup = [(1,3),(4,5),(-2,-3),(2,-1)]
-
-def key_sort(tup):
-    return tup[1]
-    
-print("List of tuple before sorting :", tup)
-print("list of tuple after sorting :", sorted(tup, key = key_sort))
